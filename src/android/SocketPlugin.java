e0822718528720f5294e6f19d325bf8855621be8
Vinicius Linck <vinnylinck@gmail.com>
2014-03-24 16:44:06 -0300

    Plugin JS file interface definition.

---
+++ src/android/SocketPlugin.java
@@ -0,0 +1,31 @@
+package com.phonegap.plugins.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class SocketPlugin extends CordovaPlugin {
+    
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        try {
+            
+            callbackContext.success();
+            return true;
+            
+        } catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            callbackContext.error(e.getMessage());
+            return false;
+        }
+    }
+}
