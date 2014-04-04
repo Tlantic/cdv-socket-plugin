bbd363269a6ea926121e9619f0a4f092c9dbd0b2
vinnylinck <vinnylinck@gmail.com>
2014-04-04 13:14:33 -0300

    IMplemented disconnectAll method. IT closes #22

--- src/windows8/SocketProxy.js
+++ src/windows8/SocketProxy.js
@@ -1,121 +1,149 @@
-/* global exports, console, Windows, require */
+    /* global exports, console, Windows, require */
 
-	//
-	exports.pool = [];
+    //
+    exports.pool = [];
 
-	//
-	exports.buildKey = function (host, port) {
-		'use strict';
-		return host.toLowerCase() + ':' + port;
-	};
+    //
+    exports.buildKey = function (host, port) {
+        'use strict';
+        return host.toLowerCase() + ':' + port;
+    };
 
-	//
-	exports.connect = function (win, fail, args) {
-		'use strict';
-		var host, port, key, socket, hostname, service;
+    //
+    exports.connect = function (win, fail, args) {
+        'use strict';
+        var host, port, key, socket, hostname, service;
 
-		// validating parameters
-		if (args.length !== 2) {
-			fail('Missing arguments for "connect" action.');
-			return;
+        // validating parameters
+        if (args.length !== 2) {
+            fail('Missing arguments for "connect" action.');
+            return;
 
-		} else {
+        } else {
 
-			// building connection key
-			host = args[0];
-			port = args[1];
-			key = exports.buildKey(host, port);
+            // building connection key
+            host = args[0];
+            port = args[1];
+            key = exports.buildKey(host, port);
 
-			// trying to recover an existing connection
-			if (exports.pool[key]) {
-				console.log('Recovered connection with ', key);
-				win(key);
-				return;
+            // trying to recover an existing connection
+            if (exports.pool[key]) {
+                console.log('Recovered connection with ', key);
+                win(key);
+                return;
 
-			} else {
+            } else {
 
-				// creating new connection;
-				hostname = new Windows.Networking.HostName(host);
-				service = port.toString();
-				socket = new Windows.Networking.Sockets.StreamSocket();
+                // creating new connection;
+                hostname = new Windows.Networking.HostName(host);
+                service = port.toString();
+                socket = new Windows.Networking.Sockets.StreamSocket();
 
-				// opening stream
-				socket.connectAsync(hostname, service).done(function () {
+                // opening stream
+                socket.connectAsync(hostname, service).done(function () {
 
-					// adding to pool
-					console.log('Connection with ', key, ' opened successfully!');
-					exports.pool[key] = socket;
+                    // adding to pool
+                    console.log('Connection with ', key, ' opened successfully!');
+                    exports.pool[key] = socket;
 
-					// returning key as success sign
-					win(key);
-				}, fail);
-			}
-		}
+                    // returning key as success sign
+                    win(key);
+                }, fail);
+            }
+        }
 
-	};
+    };
 
-	exports.disposeConnection = function(socket) {
-		'use strict';
+    exports.disposeConnection = function (socket) {
+        'use strict';
 
-		var result = false;
+        var result = false;
 
-		try {
-			socket.close();
-			result = true;
+        try {
+            socket.close();
+            result = true;
 
-		} catch(e) {
-			console.log('Unable to close connection!');
-			result= false;
+        } catch (e) {
+            console.log('Unable to close connection!');
+            result = false;
 
-		} finally {
-			return result;
-		}
-	};
+        } finally {
+            return result;
+        }
+    };
 
-	exports.disconnect = function (win, fail, args) {
-		'use strict';
+    exports.disconnect = function (win, fail, args) {
+        'use strict';
 
-		var key, socket;
+        var key, socket;
 
-		// validating parameters
-		if (args.length !== 1) {
-			fail('Missing arguments for "disconnect" action.');
-			return;
-		} else {
+        // validating parameters
+        if (args.length !== 1) {
+            fail('Missing arguments for "disconnect" action.');
+            return;
+        } else {
 
-			// retrieving existing connection
-			key = args[0];
-			socket = exports.pool[key];
-			if (!socket) {
-				fail('Connection ' + key + 'not found!');
-				return;
-			} else {
+            // retrieving existing connection
+            key = args[0];
+            socket = exports.pool[key];
+            if (!socket) {
+                fail('Connection ' + key + ' not found!');
+                return;
+            } else {
 
-				// removing from pool and closing socket
-				exports.pool[key] = undefined;
+                // removing from pool and closing socket
+                exports.pool[key] = undefined;
 
-				if (exports.disposeConnection(socket)) {
-					win();
-				} else {
-					fail('Unable to close connection with ' + key);
-				}
+                if (exports.disposeConnection(socket)) {
+                    win();
+                } else {
+                    fail('Unable to close connection with ' + key);
+                }
 
-				return;
-			}
-		}
+                return;
+            }
+        }
 
-	};
+    };
 
-	exports.disconnectAll = function (win, fail) {
-		'use strict';
-	};
+    exports.disconnectAll = function (win, fail) {
+        'use strict';
+        var socket, partial = false;
 
-	exports.send = function (win, fail, data) {
-		'use strict';
-	};
+        console.log('Preparing to disconnect all connections:');
 
-	exports.sendMessage = function (host, port, connectionId, data) {
-		'use strict';
-	};
+        // checking all connections from pool
+        for (var key in exports.pool) {
 
-	require('cordova/windows8/commandProxy').add('Socket', exports);
+            // retrieving existing socket
+            console.log('- Closing ', key, ' ...');
+            socket = exports.pool[key];
+
+            // disposing socket
+            if (!exports.disposeConnection(socket)) {
+                partial = true;
+                console.log('- Unable to close ', key);
+            }
+
+            // removing from pool
+            exports.pool[key] = undefined;
+        }
+
+
+        // returning based on all results
+        if (partial) {
+            fail('Some connections could not be closed!');
+        } else {
+            win();
+        }
+    };
+
+    exports.send = function (win, fail, data) {
+        'use strict';
+    };
+
+    exports.sendMessage = function (host, port, connectionId, data) {
+        'use strict';
+    };
+
+    require('cordova/windows8/commandProxy').add('Socket', exports);
