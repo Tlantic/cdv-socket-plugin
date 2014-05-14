20ffb06804488daba2add81dc6bd450264ed6050
felipekm <felipe.kautzmann@gmail.com>
2014-05-14 16:24:03 -0300

    Fix the following issues:#import Connection.h [#14, #16, #17, #18, #28]

--- src/ios/Connection.h
+++ src/ios/Connection.h
@@ -1,30 +1,27 @@
-
 @protocol ConnectionDelegate <NSObject>
 
--(void) sendMessage :(NSString *)host :(int)port :(NSString *)chunk;
+- (void) sendMessage : (NSString *) host : (int)port : (NSString *) chunk;
 
 @end
-
-
 
 @interface Connection : NSObject <NSStreamDelegate> {
 @private
     NSInputStream *reader;
     NSOutputStream *writer;
+    BOOL connected;
 }
 
 @property NSString *host;
 @property int port;
-
 @property (nonatomic, weak) id<ConnectionDelegate> hook;
 
+- (id) initWithNetworkAddress : (NSString*) targetHost : (int) targetPort;
+- (void) setDelegate : (id<ConnectionDelegate>) callbackRef;
+- (BOOL) isConnected;
+- (void) open;
+- (void) close;
+- (void) write : (NSString*) data;
 
-- (id)initWithNetworkAddress :(NSString*)targetHost :(int)targetPort;
-- (void)setDelegate:(id<ConnectionDelegate>)callbackRef;
-- (BOOL)isConnected;
-- (void)open;
-- (void)close;
-- (void)write :(NSString*)data;
+- (void) stream : (NSStream *) theStream handleEvent : (NSStreamEvent) streamEvent;
 
-- (void)stream:(NSStream *)theStream handleEvent:(NSStreamEvent)streamEvent;
 @end
